02d9dd2adb888d0c9d5b13caa70af4084c8a0516
Sofi <rozowasof@yandex.ru>
2025-12-09 16:36:03 +0300

    reformatted by pep8

--- app.py
+++ app.py
@@ -3,30 +3,35 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+
+@app.route("/", methods=["GET"])
 def home():
     """Главная страница"""
-    with open('templates/index.html', 'r', encoding='utf-8') as file:
-        return file.read(), 200, {'Content-Type': 'text/html; charset=utf-8'}
+    with open("templates/index.html", "r", encoding="utf-8") as file:
+        return file.read(), 200, {"Content-Type": "text/html; charset=utf-8"}
 
-@app.route('/catalog', methods=['GET'])
+
+@app.route("/catalog", methods=["GET"])
 def catalog():
     """Страница каталога"""
-    with open('templates/catalog.html', 'r', encoding='utf-8') as file:
-        return file.read(), 200, {'Content-Type': 'text/html; charset=utf-8'}
+    with open("templates/catalog.html", "r", encoding="utf-8") as file:
+        return file.read(), 200, {"Content-Type": "text/html; charset=utf-8"}
 
-@app.route('/category', methods=['GET'])
+
+@app.route("/category", methods=["GET"])
 def category():
     """Страница категории"""
-    with open('templates/category.html', 'r', encoding='utf-8') as file:
-        return file.read(), 200, {'Content-Type': 'text/html; charset=utf-8'}
+    with open("templates/category.html", "r", encoding="utf-8") as file:
+        return file.read(), 200, {"Content-Type": "text/html; charset=utf-8"}
 
-@app.route('/contacts', methods=['GET'])
+
+@app.route("/contacts", methods=["GET"])
 def contacts():
     """Страница контактов"""
-    with open('templates/contacts.html', 'r', encoding='utf-8') as file:
-        return file.read(), 200, {'Content-Type': 'text/html; charset=utf-8'}
+    with open("templates/contacts.html", "r", encoding="utf-8") as file:
+        return file.read(), 200, {"Content-Type": "text/html; charset=utf-8"}
 
-if __name__ == '__main__':
-    os.makedirs('templates', exist_ok=True)
+
+if __name__ == "__main__":
+    os.makedirs("templates", exist_ok=True)
     app.run(debug=True, port=5000)
